4bbd5f3035b6b542cffe3a4c1ffc78bff03ec7e6
Drac <altdragon@outlook.de>
2025-02-15 19:35:50 +0100

    add new merging logic

--- tag_lists/tag_list_utils.py
+++ tag_lists/tag_list_utils.py
@@ -28,10 +28,48 @@ def add_aliases(df1: pd.DataFrame, df2: pd.DataFrame):  # df1 is tags, df2 is al
     return df1
 
 
+def merge_dbr_e6_tags(dbr_df, e621_df):
+    # distinct categories for e621 for the merged df/csv (related: sd webui tagcomplete repo implementation of merged list)
+    e621_df["category"] += 7
+
+    # print the first 10 rows of the dfs
+    # print(dbr_df.head(10))
+    # print(e621_df.head(10))
+    # return None
+
+    merged_df = pd.concat([dbr_df, e621_df], ignore_index=True)
+
+    # merges both DFs and takes the first df's (danbooru) value if the same tag exists across two DFs to
+    # prevent autocomplete from getting confused which tag to use
+    ###
+    # Group by the name column (index 0) and aggregate
+    # category and post_count column values (index 1 and 2) are taken from dbr_df
+    # aliases column (index 3) is a concatenation of unique values only from both (concat aliases)
+    result_df = merged_df.groupby("name", as_index=False).agg(
+        {
+            "category": "first",  # Take the first value (from danbooru)
+            "post_count": "first",  # Take the first value (from danbooru)
+            "aliases": lambda x: ",".join(
+                sorted(
+                    set(
+                        [
+                            item.strip() for val in x.dropna() for item in val.split(",") if item.strip()
+                        ]  # Flatten and deduplicate
+                    )
+                )
+            ),  # Merge unique, non-NaN strings and remove duplicates
+        }
+    )
+
+    # Sort the result by the third column (index 2) in descending order (sort by post count)
+    result_df = result_df.sort_values(by="post_count", ascending=False)
+    return result_df
+
+
 # todo: IMPORTANT: REWORK, IGNORE BOTTOM CODE
 # actual final step kinda#
 # TODO: USE E621 HIGHER CATEGORY CSV/DATAFRAME TO MERGE WITH DBR TAGS; sd webui tagcomplete compat + cleaner
-def merge_dbr_e6_tags(df1, df2):  # merge dbr and e6 tags by name and with both aliases
+def merge_dbr_e6_tags_old(df1, df2):  # merge dbr and e6 tags by name and with both aliases
     # merge DataFrames on 'name'
     merged_df = pd.merge(df1, df2, on="name", how="outer", suffixes=("_df1", "_df2"))
 
@@ -74,4 +112,3 @@ def merge_dbr_e6_tags(df1, df2):  # merge dbr and e6 tags by name and with both 
 def remove_useless_tags():  # TODO: implement this; its clean_useless_tags.py; import from there soon:TM:?
     """remove tags that are basically meta tags, like 'bad_pixiv_id' or 'conditional dnp'"""
     pass
-
