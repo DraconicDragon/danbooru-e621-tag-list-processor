33ddb4fe7e49cec84b4a922f9251c09898120952
Drac <altdragon@outlook.de>
2025-02-15 22:28:29 +0100

    rework global_defaults.py

--- global_defaults.py
+++ global_defaults.py
@@ -5,16 +5,15 @@ DBR_ALIAS_URL = "https://danbooru.donmai.us/tag_aliases.json?commit=Search&limit
 
 DEFAULTS = {
     "choice_site": 3,
-    "min_post_thresh": 2500,  # default to 30 instead 50 cuz noobai apparently knows all artists presumably but not really?, some arent trained as much but some are
+    "min_post_thresh": 25,  # default to 25 cause noobai can represent tags with sometimes even 20 tags. 25 is a good default i think
     # problem: lower thresh = larger file, might cause performance issues with autocomplete speed (forge/a1111)? test needed
-    # "included_categories": [0,1,2,3,4,5], # kinda useless for this program so is excluded;
-    # 0: General, 1: Artist, 2: unkown?, 3: Copyright/series, 4: Character, 5: Meta DBR/ species e621, 6: disambiguation? e621, 7: Quality/Meta? e621?, 8: idk, lore e621?
-    # todo: rework this comment
+    # check the table for category index meaning here: https://github.com/DraconicDragon/dbr-e621-lists-archive/tree/main/tag-lists/danbooru_e621_merged
     "incl_aliases": "y",
     "dbr_incl_deleted_alias": "y",
-    "e6_incl_pending_alias": "n",  # shouldn't be needed
+    "e6_incl_pending_alias": "n",  # shouldn't be needed usually
     "e6_incl_deleted_alias": "y",
     "create_wildcard": 4,  # 1: DBR, 2: E6, 3: Both, 4: None
+    # NOTE: wildcard stuff not in use currently
     "wildcard_categories": [1, 2, 3],  # 1: Artist, 2: Character, 3: Copyright/Series
     # "wildcard_sorting": 2,  # 1: Alphabetical, 2: Post count # not needed with yaml wildcard
 }
