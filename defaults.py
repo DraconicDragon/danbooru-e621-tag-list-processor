a7be55fb58058155c26adbf1079e3e2ccef9de96
Drac <altdragon@outlook.de>
2025-02-28 05:09:16 +0100

    change merged_post_count_type to sum of both (3)

--- defaults.py
+++ defaults.py
@@ -5,7 +5,7 @@ DBR_ALIAS_URL = "https://danbooru.donmai.us/tag_aliases.json?commit=Search&limit
 
 DEFAULTS = {
     "choice_site": 3,
-    "merged_post_count_type": 1,  # 1: Danbooru, 2: e621, 3: Sum of Both
+    "merged_post_count_type": 3,  # 1: Danbooru, 2: e621, 3: Sum of Both
     "min_post_thresh": 25,  # default to 25 cause noobai can represent tags with sometimes even 20 tags. 25 is a good default i think
     # problem: lower thresh = larger file, might cause performance issues with autocomplete speed (forge/a1111)? test needed
     # check the table for category index meaning here: https://github.com/DraconicDragon/dbr-e621-lists-archive/tree/main/tag-lists/danbooru_e621_merged
